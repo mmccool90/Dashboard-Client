41721a3e573c08ccc833e0de905b7c54f545b701
michellemccool <mefiorella@gmail..com>
2022-04-07 16:50:04 -0400

    updates

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import OutlinedCard from "./components/weather/Weather"
+import { Grid } from '@mui/material';
+
+function App() {
+  return (
+    <Grid container spacing = {2}>
+      <Grid item xs = {4}>
+        <OutlinedCard></OutlinedCard>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default App;
